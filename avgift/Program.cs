8f2f2a9cc965f2554f6160afe9f8a9c520209ac9
Per Ekskog <per.ekskog@gmail.com>
2024-02-15 21:55:19 +0100

    Alla hus, elkostnad, formattering

--- avgift/Program.cs
+++ avgift/Program.cs
@@ -18,21 +18,21 @@ namespace Avgift
         {3, new Förbrukning {Vatten=232.767F, El=1, Städdag=0 }},
         {5, new Förbrukning {Vatten=101.623F, El=0, Städdag=0 }},
         {7, new Förbrukning {Vatten=127.427F, El=229, Städdag=0}},
-        {9, new Förbrukning {Vatten=197.35F, El=0, Städdag=0}},
-        {11, new Förbrukning {Vatten=107.06F, El=0, Städdag=0}},
-        {13, new Förbrukning {Vatten=107.084F, El=0, Städdag=0}},
-        {15, new Förbrukning {Vatten=172.095F, El=0, Städdag=0}},
-        {17, new Förbrukning {Vatten=127.524F, El=0, Städdag=0}},
-        {19, new Förbrukning {Vatten=143.459F, El=0, Städdag=0}},
-        {21, new Förbrukning {Vatten=147.626F, El=0, Städdag=0}},
-        {23, new Förbrukning {Vatten=165.706F, El=0, Städdag=0}},
-        {25, new Förbrukning {Vatten=341.832F, El=0, Städdag=0}},
-        {27, new Förbrukning {Vatten=194.535F, El=0, Städdag=0}},
-        {29, new Förbrukning {Vatten=201.311F, El=0, Städdag=0}},
+        {9, new Förbrukning {Vatten=197.35F, El=7, Städdag=0}},
+        {11, new Förbrukning {Vatten=107.06F, El=2, Städdag=0}},
+        {13, new Förbrukning {Vatten=107.084F, El=1, Städdag=0}},
+        {15, new Förbrukning {Vatten=172.095F, El=3, Städdag=0}},
+        {17, new Förbrukning {Vatten=127.524F, El=1, Städdag=0}},
+        {19, new Förbrukning {Vatten=143.459F, El=2, Städdag=0}},
+        {21, new Förbrukning {Vatten=147.626F, El=3, Städdag=0}},
+        {23, new Förbrukning {Vatten=165.706F, El=6, Städdag=0}},
+        {25, new Förbrukning {Vatten=341.832F, El=936, Städdag=0}},
+        {27, new Förbrukning {Vatten=194.535F, El=3, Städdag=0}},
+        {29, new Förbrukning {Vatten=201.311F, El=447, Städdag=0}},
         {31, new Förbrukning {Vatten=177.224F, El=0, Städdag=0}},
-        {33, new Förbrukning {Vatten=225.33F, El=0, Städdag=0}},
+        {33, new Förbrukning {Vatten=225.33F, El=4, Städdag=0}},
         {35, new Förbrukning {Vatten=140.22F, El=0, Städdag=0}},
-        {37, new Förbrukning {Vatten=110.943F, El=0, Städdag=0}},
+        {37, new Förbrukning {Vatten=110.943F, El=2, Städdag=0}},
         {39, new Förbrukning {Vatten=130.657F, El=0, Städdag=0}}
       };
 
@@ -47,8 +47,11 @@ namespace Avgift
         El_ingår = 10
       };
 
-      Console.WriteLine("Vattenkostnad");
+      Console.WriteLine("+----+-----------+-----------+-----------+-----------+-----------+");
+      Console.WriteLine("|                       Vattenkostnad                            |");
+      Console.WriteLine("+----+-----------+-----------+-----------+-----------+-----------+");
       Console.WriteLine("|Hus |Förbrukning|Rörlig     |Fast       |Total      |Justerat   |");
+      Console.WriteLine("+----+-----------+-----------+-----------+-----------+-----------+");
       foreach(var (hus,förbrukning) in avgifter_24q2)
       {
         var vatten_rörlig = förbrukning.Vatten * konstant.Vatten_rörlig_m3;
@@ -57,6 +60,24 @@ namespace Avgift
         var vatten_justerat = vatten_total - konstant.Vatten_förbetalt_år;
         Console.WriteLine(string.Format("|{0,4}|{1,11:0.00}|{2,11:0.00}|{3,11:0.00}|{4,11:0.00}|{5,11:0.00}|", hus, förbrukning.Vatten, vatten_rörlig, vatten_fast, vatten_total, vatten_justerat));
       }
+      Console.WriteLine("+----+-----------+-----------+-----------+-----------+-----------+");
+
+      Console.WriteLine();
+
+      Console.WriteLine("+----+-----------+-----------+-----------+-----------+");
+      Console.WriteLine("|                  Elkostnad                         |");
+      Console.WriteLine("+----+-----------+-----------+-----------+-----------+");
+      Console.WriteLine("|Hus |Förbrukning|Ingår      |Justerad   |Total      |");
+      Console.WriteLine("+----+-----------+-----------+-----------+-----------+");
+      foreach(var (hus,förbrukning) in avgifter_24q2)
+      {
+        var el_justerad = förbrukning.El - konstant.El_ingår;
+        var el_total = el_justerad * konstant.El_rörlig_kWh;
+        if(el_total < 0) el_total = 0;
+        Console.WriteLine(string.Format("|{0,4}|{1,11:0.00}|{2,11:0.00}|{3,11:0.00}|{4,11:0.00}|", hus, förbrukning.El, konstant.El_ingår, el_justerad, el_total));
+      }
+      Console.WriteLine("+----+-----------+-----------+-----------+-----------+");
+
     }
   }
 }
