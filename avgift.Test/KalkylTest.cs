86c8b2640be31153a23eb1331590cc3958ff3e8d
Per Ekskog <per.ekskog@gmail.com>
2024-02-20 18:26:29 +0100

    Add first UT.

---
+++ avgift.Test/KalkylTest.cs
@@ -0,0 +1,36 @@
+using Avgift;
+
+namespace avgift.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void CalculateVatten_AllOtherShouldBeZero()
+    {
+        var kalkyl = new Avgift.Kalkyl();
+        var förbrukning = new Avgift.Förbrukning
+        {
+            Vatten = 1,
+            El = 8,
+            Städdag = 0
+        };
+        var konstant = new Konstant
+        {
+            Avgift_kvartal = 1640,
+            Fondering_kvartal = 280,
+            Moms_ut = 0.25,
+            Vatten_rörlig_m3 = 16,
+            Vatten_fast_år = 12,
+            Vatten_moms = 0.25,
+            Vatten_förbetalt_år = 5,
+            El_rörlig_kWh = 0.8,
+            El_ingår = 10,
+            El_moms = 0.25,
+            Städdag_hus = 160,
+            Städdag_moms = 0.25
+        };
+        var kostnad = kalkyl.Vatten(förbrukning, konstant, new Kostnad());
+        var expected = new Kostnad() { Vatten_rörlig = 16, Vatten_fast = 12, Vatten_brutto = 28, Vatten_netto = 23, El_justerad = 0, El_netto = 0, Städdag_netto = 0, Moms = 0, AttBetala = 0 };
+        Assert.Equivalent(kostnad, expected);
+    }
+}
