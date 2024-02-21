fdd5a6987d2136a534053688b0d7cca5c34e0087
Per Ekskog <per.ekskog@gmail.com>
2024-02-21 12:26:46 +0100

    Use top level statements.

--- avgift/Program.cs
+++ avgift/Program.cs
@@ -1,43 +1,33 @@
-﻿namespace Avgift
+﻿var repository = new Avgift.Repository();
+
+var förbrukning = repository.Förbrukning("24q2");
+var konstant = repository.Konstant("24q2");
+var inbetalning = repository.Inbetalning("24q2");
+
+var kalkyl = new Avgift.Kalkyl();
+var algoritm = new Avgift.Algorithm();
+
+var kostnad = new Dictionary<int, Avgift.Kostnad>();
+int[] hus = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39];
+foreach (var h in hus)
 {
-  class Program
-  {
-    static void Main(string[] args)
-    {
-      var repository = new Repository();
-
-      var förbrukning = repository.Förbrukning("24q2");
-      var konstant = repository.Konstant("24q2");
-      var inbetalning = repository.Inbetalning("24q2");
-
-      var kalkyl = new Kalkyl();
-      var algoritm = new Algorithm();
-
-      var kostnad = new Dictionary<int, Kostnad>();
-      int[] hus = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39];
-      foreach (var h in hus)
-      {
-        kostnad[h] = algoritm.Kostnad(kalkyl, h, förbrukning, konstant);
-      }
-
-      var verifikation = new Dictionary<string, Verifikation>();
-      foreach (var (v, hl) in inbetalning)
-      {
-        verifikation[v] = algoritm.Verifikation(kalkyl, hl, konstant, kostnad);
-      }
-
-      var printer = new Printer();
-
-      printer.PrintVattenkostnad(hus, förbrukning, kostnad, konstant);
-      Console.WriteLine();
-      printer.PrintElkostnad(hus, förbrukning, kostnad, konstant);
-      Console.WriteLine();
-      printer.PrintStäddag(hus, kostnad);
-      Console.WriteLine();
-      printer.PrintAttBetala(hus, kostnad, konstant);
-      Console.WriteLine();
-      printer.PrintBokföring(hus, kostnad, konstant, verifikation, inbetalning);
-
-    }
-  }
+  kostnad[h] = algoritm.Kostnad(kalkyl, h, förbrukning, konstant);
 }
+
+var verifikation = new Dictionary<string, Avgift.Verifikation>();
+foreach (var (v, hl) in inbetalning)
+{
+  verifikation[v] = algoritm.Verifikation(kalkyl, hl, konstant, kostnad);
+}
+
+var printer = new Avgift.Printer();
+
+printer.PrintVattenkostnad(hus, förbrukning, kostnad, konstant);
+Console.WriteLine();
+printer.PrintElkostnad(hus, förbrukning, kostnad, konstant);
+Console.WriteLine();
+printer.PrintStäddag(hus, kostnad);
+Console.WriteLine();
+printer.PrintAttBetala(hus, kostnad, konstant);
+Console.WriteLine();
+printer.PrintBokföring(hus, kostnad, konstant, verifikation, inbetalning);
